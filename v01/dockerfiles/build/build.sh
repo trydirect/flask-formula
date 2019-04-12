a05263fe1073d0bb401fe84fbf6004cb93f30b8d
Cristin <cristin@optimum-web.com>
2019-04-12 09:58:52 +0300

    remove unecessary bash script

--- v01/dockerfiles/build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-VERSION="1.0.2"
-docker-compose build
-docker tag build_web:latest optimum/development:flask-formula-$VERSION-s
-docker push optimum/development:flask-formula-$VERSION-s
